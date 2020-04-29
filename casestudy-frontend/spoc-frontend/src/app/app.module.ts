e542d5fc01f9772fdc88806a69266f83b7f30b79
darshansp8 <d.sakpal38@gmail.com>
2020-04-29 23:09:06 +0530

    Added css to the navbar

--- casestudy-frontend/spoc-frontend/src/app/app.module.ts
+++ casestudy-frontend/spoc-frontend/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RequestsComponent } from './requests/requests.component';
 import { ViewRequestsComponent } from './view-requests/view-requests.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,8 @@ import { ViewRequestsComponent } from './view-requests/view-requests.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
