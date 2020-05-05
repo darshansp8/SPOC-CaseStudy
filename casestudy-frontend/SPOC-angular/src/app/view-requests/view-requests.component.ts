7d35e35a022e6f94fcbc29e712484481f3d3f164
darshansp8 <d.sakpal38@gmail.com>
2020-05-06 02:04:35 +0530

    Added update button to update the requests

--- casestudy-frontend/SPOC-angular/src/app/view-requests/view-requests.component.ts
+++ casestudy-frontend/SPOC-angular/src/app/view-requests/view-requests.component.ts
@@ -17,6 +17,7 @@ export class ViewRequestsComponent implements OnInit {
   isCheckedRoom: boolean;
   trainerSet: boolean;
   roomSet: boolean;
+  show: boolean = false;
 
  //For Trainers of particlaur technology
   public techTrainers: any=[];
@@ -46,9 +47,12 @@ export class ViewRequestsComponent implements OnInit {
   public room_capacity: any;
 
   public singleRequest: any=[];
+  public requestArray:any = {};
   requestId:number;
   verticalId:number;
+  verticalName:string
   technologyId:number;
+  technologyName:string;
   pmName:string;
   candidateCount:number;
   trainingMode:string;
@@ -70,7 +74,7 @@ export class ViewRequestsComponent implements OnInit {
     this.api.getRequestById(this.request_id)
     .subscribe(data => 
       this.singleRequest = data);
-    console.log(this.singleRequest);
+    // console.log(this.singleRequest);
 
     let tid = this.route.snapshot.paramMap.get('tid');
     this.technology_id = tid;
@@ -85,6 +89,27 @@ export class ViewRequestsComponent implements OnInit {
     .subscribe(data => {
       this.rooms = Array.from(Object.keys(data),k=>data[k]);
     });
+  }
+
+
+  editRequest(){
+
+    this.show =true;
+
+    this.requestArray.requestId = this.singleRequest.requestId;
+    this.requestArray.verticalName = this.singleRequest.vertical[0].verticalName;
+    this.requestArray.pmName = this.singleRequest.pmName;
+    this.requestArray.technologyName = this.singleRequest.technology[0].technologyName;
+    this.requestArray.trainingMode = this.singleRequest.trainingMode;
+    this.requestArray.candidateCount = this.singleRequest.candidateCount;
+    this.requestArray.trainingStartDate = this.singleRequest.trainingStartDate;
+    this.requestArray.trainingEndDate = this.singleRequest.trainingEndDate;
+    this.requestArray.trainingStartTime = this.singleRequest.trainingStartTime;
+    this.requestArray.trainingEndTime = this.singleRequest.trainingEndTime;
+  }
+
+  updateRequest(){
+    console.log(this.singleRequest);
   }
 
   selectCheckboxTrainer(e){
