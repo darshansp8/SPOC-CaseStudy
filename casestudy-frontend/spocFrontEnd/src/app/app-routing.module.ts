9663d7e434e4e24741120e4b0a5750c5b925e231
darshansp8 <d.sakpal38@gmail.com>
2020-05-07 20:00:52 +0530

    Final approval from spoc.

---
+++ casestudy-frontend/spocFrontEnd/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { RequestsComponent } from './requests/requests.component';
+import { ViewRequestsComponent } from './view-requests/view-requests.component';
+
+
+const routes: Routes = [
+  {path: '', redirectTo: 'pendingrequests', pathMatch: 'full'},
+  {path: "pendingrequests", component: RequestsComponent},
+  {path: "viewrequest/:id/:tech/:cc", component: ViewRequestsComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
